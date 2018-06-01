3db18819526caf984bd58f37be6f0cf558ac0bfc
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2018-06-01 04:09:53 -0300

    Acrescentado função de enviar mensagens, adicionar contato, listagem de conversas com bugs.

---
+++ app/src/main/java/whatsappclone/app/rodrigo/whatsappclone/Model/Conversas.java
@@ -0,0 +1,38 @@
+package whatsappclone.app.rodrigo.whatsappclone.Model;
+
+public class Conversas {
+
+    private String idUsuario;
+    private String nome;
+    private String mensagem;
+
+    public Conversas(){
+
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
+
+
